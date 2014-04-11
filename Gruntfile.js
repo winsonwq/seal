6f4cb61b9668b42e6c82612e6de3667a891b3eae
Wang Qiu <winsonwq@gmail.com>
2014-04-11 12:06:26 +0800

    init page

---
+++ Gruntfile.js
@@ -0,0 +1,114 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    buildPath: 'public',
+    devPath: 'dev',
+    clean: ['<%=buildPath%>', '<%=devPath%>', '.tmp', '.temp'],
+    jade: {
+      options: {
+        client: false
+      },
+      temp: {
+        src: ['views/index.jade'],
+        dest: '.temp',
+        options: {
+          pretty: true
+        }
+      }
+    },
+    connect: {
+      dev: {
+        options: {
+          port: 9001,
+          base: ['dev', 'bower_components'],
+          livereload: 35729,
+          keepalive: true
+        }
+      },
+      build: {
+        options: {
+          port: 9002,
+          base: ['public', 'bower_components'],
+          keepalive: true
+        }
+      }
+    },
+    watch: {
+      views: {
+        files: ['views/**/*.jade'],
+        tasks: ['jade:dev'],
+        options: {
+          livereload: 35729
+        }
+      },
+      styles: {
+        files: ['styles/**/*.styl'],
+        tasks: ['stylus:dev'],
+        options: {
+          livereload: 35729
+        }
+      }
+    },
+    concurrent: {
+      dev: {
+        tasks: ['watch', 'connect:dev']
+      }
+    },
+    stylus: {
+      temp: {
+        files: {
+          '.temp/styles/index.css': 'styles/index.styl',
+          '.temp/styles/components/header.css': 'styles/components/header.styl',
+          '.temp/styles/components/agent.css': 'styles/components/agent.styl',
+          '.temp/styles/components/resource.css': 'styles/components/resource.styl'
+        }
+      }
+    },
+    useminPrepare: {
+      build: {
+        src: '.temp/index.html',
+        options: {
+          dest: '.temp'
+        }
+      }
+    },
+    usemin: {
+      html: '.temp/index.html'
+    },
+    copy: {
+      dev: {
+        files: [
+          { expand: true, cwd: '.temp', src : './styles/**', dest: 'dev/'},
+          { src : '.temp/index.html', dest: 'dev/index.html' }
+        ]
+      },
+      build: {
+        files: [
+          { src : '.temp/styles/main.css', dest: 'public/styles/main.css'},
+          { src : '.temp/index.html', dest: 'public/index.html' }
+        ]
+      }
+    }
+  });
+
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.registerTask('dev', [
+    'clean',
+    'jade:temp',
+    'stylus:temp',
+    'copy:dev',
+    'concurrent:dev'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean',
+    'jade:temp',
+    'stylus:temp',
+    'useminPrepare',
+    'concat',
+    'cssmin',
+    'usemin',
+    'copy:build'
+  ]);
+};
