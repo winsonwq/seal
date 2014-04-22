46d19da258eec7cfb20401f16aee198ed6930cae
Wang Qiu <winsonwq@gmail.com>
2014-04-23 01:23:53 +0800

    finally config using relative path in production mode

--- tasks/usemin.js
+++ tasks/usemin.js
@@ -1,15 +1,47 @@
 module.exports = function gruntUsemin (grunt) {
 
+  var PATH = require('path');
+
   grunt.config('usemin', {
     html: '.temp/pages/**/*.html'
   });
 
   grunt.config('useminPrepare', {
     build: {
-      src: '.temp/pages/**/*.html',
-      options: {
-        dest: '.temp',
-        root: '.temp'
+      files: [{ src: '.temp/pages/**/*.html' }]
+    },
+    options: {
+      // destination folder
+      dest: '.temp',
+      flow: {
+        steps: {'css': ['concat', 'cssmin'], 'js' : ['concat', 'uglifyjs'] },
+        post: {
+          'css': [{
+            name: 'concat',
+            createConfig: function (context, block) {
+              var generated = context.options.generated;
+              generated.files[0].dest = PATH.resolve(context.inDir, block.dest);
+            }
+          }, {
+            name: 'cssmin',
+            createConfig: function (context, block) {
+              var generated = context.options.generated;
+              generated.files[0].src = generated.files[0].dest = PATH.resolve(context.inDir, block.dest);
+            }
+          }],
+          'js': [{
+            name: 'concat',
+            createConfig: function(context, block) {
+              context.options.generated.files[1].dest = PATH.resolve(context.inDir, block.dest);
+            }
+          }, {
+            name: 'uglify',
+            createConfig: function (context, block) {
+              var generated = context.options.generated;
+              generated.files[0].src = generated.files[0].dest = PATH.resolve(context.inDir, block.dest);
+            }
+          }]
+        }
       }
     }
   });
